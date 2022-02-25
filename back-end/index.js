149a5f889db3e5ed7bcee8616a723801d05ccc19
asatriangrish <68015194+asatriangrish@users.noreply.github.com>
2022-02-25 18:25:44 +0400

    feat: calculate HOL TWAP

--- back-end/index.js
+++ back-end/index.js
@@ -1,36 +1,30 @@
 const { default: axios } = require("axios");
-
+const BN = require('bn.js')
 
 async function getTWAP() {
     var config = {
         method: 'get',
-        url: 'https://api.zilstream.com/rates/HOL?interval=5m&period=1h',
+        url: 'https://api.zilstream.com/rates/HOL?interval=15m&period=1h&currency=USD',
         headers: {}
     };
 
     try {
         let result = await axios(config);
-        console.log(result.data);
+        let prices = result.data;
+        let sum = 0;
+        for(let priceData of prices) {
+            let price = priceData["close"];
+            sum += price;
+        }
+        return sum/5;
     } catch (e) {
         console.log("error occured from ZilStream===========>", e);
     }
 
 }
 
-async function getZilPrice() {
-    var config = {
-        method: 'get',
-        url: 'https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest?convert=USD&symbol=ZIL',
-        headers: {
-            'X-CMC_PRO_API_KEY': '60a32cab-af95-4f29-82ef-8bb3c64b179f'
-        }
-    };
-    try {
-        let result = await axios(config);
-        let price = result.data.data["ZIL"][0]["quote"]["USD"]["price"];
-        console.log(price);
-
-    } catch(e) {
-        console.log("Error occured from CMC=============>", e)
-    }
+async function main() {
+    let result = await getTWAP();
+    console.log(result);
 }
+main();
